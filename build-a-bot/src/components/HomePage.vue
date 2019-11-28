536ab89c821ae691905fc336220ce4e4577f08b8
Mohammad Shalabi <mohammad.shalabi@flightright.de>
2019-11-28 15:24:31 +0100

    init project

---
+++ build-a-bot/src/components/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png"  aria-hidden="true" />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HomePage extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+.get-started {
+  padding-top: 20px;
+  font-size: 25px;
+}
+</style>
--- build-a-bot/tests/unit/example.spec.ts
+++ build-a-bot/tests/unit/example.spec.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HomePage from '@/components/HomePage.vue';
 
-describe('HelloWorld.vue', () => {
+describe('HomePage.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(HomePage, {
       propsData: { msg },
     });
     expect(wrapper.text()).to.include(msg);
